5e876aa4ccf7ba28974ef00c0baf9ebcbdd4ad6b
AhmedRaafat14 <ahmed.raafat1412@gmail.com>
2018-01-24 20:34:05 +0200

    add problem 1.7 of ch 1

---
+++ Chapter 1/1-7.py
@@ -0,0 +1,27 @@
+def change_to_zero(matrix):
+    n = len(matrix)
+    if n == 0:
+        return matrix
+    m = len(matrix[0])
+
+    zero_rows = zero_cols = []
+    for row in range(n):
+        for col in range(m):
+            if matrix[row][col] == 0:
+                zero_rows.append(row)
+                zero_cols.append(col)
+
+    for row in range(n):
+        for col in range(m):
+            if row in zero_rows or col in zero_cols:
+                matrix[row][col] = 0
+
+    # for row in range(n):
+    #     for col in zero_cols:
+    #         matrix[row][col] = 0
+
+    return matrix
+
+if __name__ == "__main__":
+    print( change_to_zero([[1, 2, 3], [4, 0, 5], [6, 7, 8]]) )
+    print( change_to_zero([[1,1,1,1,1],[1,0,1,1,1],[1,1,1,1,1],[1,1,1,0,1],[2,3,4,5,6]]) )
