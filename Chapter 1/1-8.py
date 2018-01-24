c0c83bc58a2793156397795ee1d8167d8a8b9802
AhmedRaafat14 <ahmed.raafat1412@gmail.com>
2018-01-24 20:58:14 +0200

    add problem 1.8 of ch 1

---
+++ Chapter 1/1-8.py
@@ -0,0 +1,33 @@
+'''
+Assume you have a method isSubstring which checks if one word is a
+substring of another. Given two strings, si and s2, write code to check if s2 is
+a rotation of si using only one call to isSubstring (e.g.,"waterbottle"is a rota-
+tion of "erbottlewat")
+'''
+
+def is_rotation(s1, s2):
+    if len(s1) == len(s2):
+        return False
+
+    return isSubstring(s1+s1, s2)
+
+def isSubstring(s1, s2):
+    # Fastest solution
+    # return s2 in s1
+
+    if len(s2) > len(s1):
+        return False
+
+    for i in range(len(s1) - len(s2) - 1):
+        is_sub = 0
+        for j in range(len(s2)):
+            if s1[i + j] != s2[j]:
+                is_sub = 1
+                break
+
+    if is_sub:
+        return True
+    return False
+
+if __name__ == "__main__":
+    print( is_rotation('waterbottle', 'erbottlewa') )
