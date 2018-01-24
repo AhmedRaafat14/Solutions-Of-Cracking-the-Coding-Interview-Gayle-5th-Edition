33219fde3ffc99b0fb20bc74e2f1c132c6b07523
AhmedRaafat14 <ahmed.raafat1412@gmail.com>
2018-01-24 20:08:48 +0200

    add problem 1.6 of ch 1

---
+++ Chapter 1/1-6.py
@@ -0,0 +1,33 @@
+def rotate_matrix(m):
+    n = len(m)
+    rotated_m = [None] * n
+    for r in range(n):
+        rotated_m[r] = [None] * n
+
+    for row in range(n):
+        for col in range(n):
+            rotated_m[n - col - 1][row] = m[row][col]
+
+    return rotated_m
+
+def rotate_matrix_in_place(m):
+  n = len(m)
+
+  for col in range(n//2):
+    for row in range(col, n - col - 1):
+      temp1 = m[n - col - 1][row]
+      m[n - col - 1][row] = m[row][col]
+
+      temp2 = m[n - row - 1][n - col - 1]
+      m[n - row - 1][n - col - 1] = temp1
+
+      temp1 = m[col][n - row - 1]
+      m[col][n - row - 1] = temp2
+      m[row][col] = temp1
+
+  return m
+
+
+if __name__ == "__main__":
+    print(rotate_matrix([[1, 2], [3, 4]]))
+    print(rotate_matrix_in_place([[1, 2], [3, 4]]))
