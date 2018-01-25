36b256a08bc98cfc7fe812080f23fb4b0398c702
AhmedRaafat14 <ahmed.raafat1412@gmail.com>
2018-01-25 20:44:51 +0200

    edit problem 1.1 and 1.3 of ch 1

--- Chapter 1/1-1.py
+++ Chapter 1/1-1.py
@@ -3,23 +3,29 @@ Implement an algorithm to determine if a string has all unique characters. What
 if you cannot use additional data structures?
 '''
 
-s = input()
-
-flag = True
-
 # First approch with Hash Map DS
-# uniq_chs = {}
-# for ch in s:
-#     if ch in uniq_chs:
-#         flag = False
-#         break
-#     else:
-#         uniq_chs[ch] = 1
+def check_unique_chars_using_map(S):
+    uniq_chs = {}
+    flag = True
+    for ch in S:
+        if ch in uniq_chs:
+            flag = False
+            break
+        else:
+            uniq_chs[ch] = 1
+
+    return True if flag else False
 
 # Second approch without any DS
-for ch in s:
-    if s.count(ch) > 1:
-        flag = False
-        break
+def check_unique_chars_without_map(S):
+    flag = True
+    for ch in S:
+        if S.count ( ch ) > 1:
+            flag = False
+            break
 
-print('yes' if flag else 'no')
+    return True if flag else False
+
+if __name__ == "__main__":
+    print( check_unique_chars_using_map("ahmed") )
+    print( check_unique_chars_without_map("ahmad") )
--- Chapter 1/1-3.py
+++ Chapter 1/1-3.py
@@ -2,18 +2,38 @@
 Given two strings, write a method to decide if one is a permutation of the other.
 '''
 
-s1 = input()
-s2 = input()
+def is_permute(S1, S2):
+    counter = {}
 
-# Approache 1
-# print('yes' if sorted(s1) == sorted(s2) else 'no')
+    for ch in S1:
+        if ch not in counter:
+            counter[ch] = 1
+            continue
+        counter[ch] += 1
 
-# Approache 2
-flag = 1
-for i in range(len(s1)):
-    if s1.count(s1[i]) != s2.count(s1[i]):
-        flag = 0
-        break
+    for ch in S2:
+        if ch not in counter:
+            return  False
+        counter[ch] -= 1
+        if counter[ch] == 0:
+            del counter[ch]
+
+    if len(counter) == 0:
+        return True
+
+def is_permute_simple(S1, S2):
+    return True if sorted(S1) == sorted(S2) else False
+
+def is_permute_simple_counter(S1, S2):
+    for i in range(len(S1)):
+        if S1.count(S1[i]) != S2.count(S1[i]):
+            return False
+
+    return True
 
 
-print('yes' if flag else 'no')
+if __name__ == "__main__":
+    print( is_permute('ahmed', 'ahmed') )
+    print( is_permute_simple_counter('ahmed', 'ahmed') )
+    print( is_permute('ahmed', 'ahmad') )
+    print( is_permute_simple('ahmed', 'ahmad') )
