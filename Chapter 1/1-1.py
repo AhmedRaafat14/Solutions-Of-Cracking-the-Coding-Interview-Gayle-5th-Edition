e5fbf337996d6395666a332ac412f1cfa47b694c
Ahmed Raafat <a.raafat@global.leaseweb.com>
2018-08-01 18:52:53 +0200

    Update problem solutions

--- Chapter 1/1-1.py
+++ Chapter 1/1-1.py
@@ -1,31 +1,43 @@
-'''
+"""
 Implement an algorithm to determine if a string has all unique characters. What
 if you cannot use additional data structures?
-'''
+"""
 
-# First approch with Hash Map DS
-def check_unique_chars_using_map(S):
+
+# First approach with Hash Map DS
+def check_unique_chars_using_map(s):
     uniq_chs = {}
-    flag = True
-    for ch in S:
+
+    for ch in s:
         if ch in uniq_chs:
-            flag = False
-            break
-        else:
-            uniq_chs[ch] = 1
+            return False
 
-    return True if flag else False
+        uniq_chs[ch] = 1
 
-# Second approch without any DS
-def check_unique_chars_without_map(S):
-    flag = True
-    for ch in S:
-        if S.count ( ch ) > 1:
-            flag = False
-            break
+    return True
 
-    return True if flag else False
+
+# Second approach without any DS
+def check_unique_chars_without_map(s):
+    for ch in s:
+        if s.count(ch) > 1:
+            return False
+    return True
+
+
+# Another approaches
+def check_unique_using_list_set(s):
+    if list(set(s)) == list(s):
+        return True
+    return False
+
 
 if __name__ == "__main__":
-    print( check_unique_chars_using_map("ahmed") )
-    print( check_unique_chars_without_map("ahmad") )
+    print(check_unique_chars_using_map("ahmed"))
+    print(check_unique_chars_using_map("aaahmed"))
+
+    print(check_unique_chars_without_map("ahmed"))
+    print(check_unique_chars_without_map("ahmmad"))
+
+    print(check_unique_using_list_set("ahmed"))
+    print(check_unique_using_list_set("aaahmed"))
